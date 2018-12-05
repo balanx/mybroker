60343bf09710717ea6f8b0e9377dd5bdba69b0a7
balanx <tobalanx@qq.com>
2018-12-06 00:30:48 +0800

    add: show and edit TypePrice()

--- cond.py
+++ cond.py
@@ -8,8 +8,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+#from kivy.properties import BooleanProperty, NumericProperty, ListProperty, DictProperty
 
-
+import datetime as dt
 import common, typeprice, typetime
 
 
@@ -19,29 +20,40 @@ class TypeDropDown(DropDown):
         super().__init__(**kwargs)
         self.root = root
 
+
     def on_select(self, data):
         self.attach_to.text = data
         self.root.ids.layout.remove_widget(self.root.type)
         if data == 'Price':
-            self.root.type = typeprice.TypePrice(self.root.cond)
+            self.root.type = typeprice.TypePrice(self.root.subcond)
         else:
-            self.root.type = typetime.TypeTime(self.root.cond)
+            self.root.type = typetime.TypeTime(self.root.subcond)
 
         self.root.ids.layout.add_widget(self.root.type)
 
-class CondScreen(Screen):
 
-    def __init__(self, cond, **kwargs):
+class CondScreen(Screen):
+    def __init__(self, index, cond, **kwargs):
         super().__init__(**kwargs)
+        #self.index = index
         self.cond = cond
-        self.ids.btn_enable.text = "~" if self.cond['enable'] else '|'
-        self.type = Label(text='None')
+        self.subcond = cond[index]
+        self.ids.btn_enable.text = "~" if cond[0] else '|'
+
+        if self.subcond[0] == 1:
+            self.type = typetime.TypeTime(self.subcond)
+            self.ids.btn_type.text = 'Time'
+        elif self.subcond[0] == 2:
+            self.type = typeprice.TypePrice(self.subcond)
+            self.ids.btn_type.text = 'Price'
+        else:
+            self.type = Label(text='None')
+
         self.ids.layout.add_widget(self.type)
 
     def toggle_enable(self, button):
-        self.cond['enable'] = not self.cond['enable']
-        button.text = "~" if self.cond['enable'] else '|'
-
+        self.cond[0] = not self.cond[0]
+        button.text = "~" if self.cond[0] else '|'
 
 
 
@@ -50,12 +62,17 @@ class TestApp(App):
     def build(self):
         self.row_height = Window.height / 10
         self.row_space = 10
-        self.cond = common.init_cond()
+        self.index = 1
+        # Type definition
+        # 1: Time
+        # 2: Price
+        self.cond = [False, [0, None, None, None], [1, dt.date.today(), 0, None], [2, 5, False, 1.0]]
 
-        return CondScreen(self.cond)
+        return CondScreen(self.index, self.cond)
+
 
 if __name__ == '__main__':
-    
+
     kv = Builder.load_file("./cond.kv")
     TestApp().run()
 
--- typeprice.py
+++ typeprice.py
@@ -9,14 +9,43 @@ class TypePrice(BoxLayout):
     def __init__(self, cond, **kwargs):
         super().__init__(**kwargs)
         self.cond = cond
+        cond[0] = 2
 
-    def on_checkbox1_active(self, instance, text):
-        if instance.active:
-            print(text)
+        if cond[1] == 1:
+            self.ids.cb_abs.active = True
+        elif cond[1] == 2:
+            self.ids.cb_open.active = True
+        elif cond[1] == 3:
+            self.ids.cb_close.active = True
+        elif cond[1] == 4:
+            self.ids.cb_max.active = True
+        elif cond[1] == 5:
+            self.ids.cb_min.active = True
 
-    def on_checkbox2_active(self, instance, text):
+        if cond[2]:
+            self.ids.cb_more.active = True
+        else:
+            self.ids.cb_less.active = True
+
+        self.ids.txi_val.text = str(cond[3])
+
+
+    def on_checkbox_active(self, instance, n):
         if instance.active:
-            print(text)
+            if n < 10:
+                self.cond[1] = n
+            elif n == 10: # more
+                self.cond[2] = True
+            elif n == 11: # less
+                self.cond[2] = False
+
+
+
+    def on_text_val(self, text):
+        try:
+            self.cond[3] = float(text)
+        except:
+            self.cond[3] = 0.0
 
 
 #
